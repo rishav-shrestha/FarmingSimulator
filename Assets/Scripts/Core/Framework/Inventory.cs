782232d0c064b661e123114c84e93bc5abb45b08
rishav-shrestha <grish003@edu.xamk.fi>
2025-12-08 10:44:46 +0200

    Fix some bugs

--- Assets/Scripts/Core/Framework/Inventory.cs
+++ Assets/Scripts/Core/Framework/Inventory.cs
@@ -58,10 +58,6 @@ public class Inventory : MonoBehaviour
             int index = _cropDatabase.crops.IndexOf(selectedCrop);
             index = (index + 1) % _cropDatabase.crops.Count;
             SetSelectedCrop( _cropDatabase.crops[index]);
-            if (_cropDatabase.crops[index].locked)
-            {
-                CycleSelectedCrop();
-            }
         }
     }
     public void CycleSelectedCrop(WorkerInteraction worker)
@@ -72,10 +68,6 @@ public class Inventory : MonoBehaviour
             int index = _cropDatabase.crops.IndexOf(worker.selectedcrop);
             index = (index + 1) % _cropDatabase.crops.Count;
             worker.SetCrop( _cropDatabase.crops[index]);  
-            if (_cropDatabase.crops[index].locked)
-            {
-                CycleSelectedCrop(worker);
-            }
         }
     }
     
